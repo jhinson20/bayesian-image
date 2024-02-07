74a08aa0670fdd02ed323b4558a6b132123a6dc1
Jordan Hinson <jhinson2012@gmail.com>
2024-02-07 17:16:56 -0500

    Created the grid and made the buttons toggleable

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import tkinter as tk
+from tkinter import PhotoImage
 
 def button_press(m):
-    print("something: " + m)
+    row = int(m[:m.index(',')])
+    col = int(m[m.index(',') + 1:])
+    #buttons[row][col] = 1
+    if matrix[row][col] == 1:
+        matrix[row][col] = 0
+        buttons[row][col].config(bg="grey")
+    else:
+        matrix[row][col] = 1
+        buttons[row][col].config(bg="red")
 
+numberRows = 5
+numberCols = 5
 window = tk.Tk()
 window.title("Shapes")
 window.geometry("500x400")
@@ -18,15 +29,19 @@ buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
 buttonFrame.columnconfigure(3, weight=1)
-
 buttonFrame.place(in_=rightFrame, anchor="c", relx=.5, rely=.5)
+buttons = [[0 for x in range(numberRows)] for y in range(numberCols)]
+matrix = [[0 for x in range(numberRows)] for y in range(numberCols)]
 
-buttons = [[0 for x in range(4)] for y in range(4)]
-for i in range(4):
-    for j in range(4):
-        test = str(i) + " " + str(j)
-        buttons[i][j] = tk.Button(buttonFrame, text = ("Button " + str((i+1)*(j+1))), command=lambda m=test: button_press(m))
+
+redCircle = PhotoImage(file="images/red_circle.png")
+whiteCircle = PhotoImage(file="images/white_circle.png")
+
+for i in range(len(buttons)):
+    for j in range(len(buttons[0])):
+        rowCol = str(i) + ',' + str(j)
+        #buttons[i][j] = tk.Button(buttonFrame, text = ("Button " + str((i+1)*(j+1))), command=lambda m=test: button_press(m), image=whiteCircle, height=50, width=50)
+        buttons[i][j] = tk.Button(buttonFrame, command=lambda m=rowCol: button_press(m), height=2, width=5, bg="grey")
         buttons[i][j].grid(column=j,row=i, pady=10, padx=10)
 
 window.mainloop()
-
