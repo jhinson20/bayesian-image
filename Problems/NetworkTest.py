ca1d4e95faf6c125c62b49b132dd27219dc02269
Jordan Hinson <jordan.hinson@g.fmarion.edu>
2024-03-19 20:02:06 -0400

    Simplified some code, renamed file, and added some comments

--- Problems/NetworkTest.py
+++ Problems/NetworkTest.py
@@ -1,22 +1,24 @@
-#I will be testing properties of the dotline dataset using a network with the probability of shape being left at 60/40
-
-import tkinter as tk
 import pandas as pd
 import os
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
 from pgmpy.factors.discrete.CPD import TabularCPD
 from pgmpy.estimators import BayesianEstimator
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+import numpy as np
 
 #Set global variable values
 
 numberNodes = 4
 sampleSize = 1000
-numberOfRuns = 5
+numberSplits = 5
 seed = 84
-_confusion = [[0, 0], [0, 0]]
+uniformShape = False
+_accuracy = []
+_sensitivity = []
+_specificity = []
+# _confusion = [[0, 0], [0, 0]]
 
 #Creates file object
 fileName = 'data/dotLine.csv'
@@ -29,81 +31,90 @@ if not os.path.isfile(fileName):
 #Randomly select data
 data = dataFile.sample(n=sampleSize, random_state=seed, replace=True)
 
-#Split data
-train, test = train_test_split(data, test_size=.2, random_state=seed)
-
 #Creates the bayesian network
 edges = [('shape', 'x0'), ('shape', 'x1'), ('shape', 'x2'), ('shape', 'x3')]
 
 graph = BayesianNetwork(edges)
-graph.fit(train, state_names={'shape': [0, 1], 'x0': [0, 1], 'x1': [0, 1], 'x2': [0, 1], 'x3': [0, 1]}, 
-          estimator=BayesianEstimator, prior_type='BDeu', complete_samples_only=False)
 
-#Makes the shape estimation uniform
-#shape_cpd = TabularCPD('shape', 2, [[0.5],[0.5]])
-#graph.add_cpds(shape_cpd)
+#Create the folds for the data
+kf = KFold(n_splits=numberSplits, shuffle=False)
 
-infer = VariableElimination(graph)
+for foldNum, (train_index, test_index) in enumerate(kf.split(data), 1):
 
-#Arrays that will contain the actual and predicted values of each of the rows from the test set
-pred_test = []
-actu_test = []
+    #Select training and test data based on the indicies of the fold
+    train = data.iloc[train_index]
+    test = data.iloc[test_index]
 
-#Evaluating test data
+    #Fit data to network
+    graph.fit(train, state_names={'shape': [0, 1], 'x0': [0, 1], 'x1': [0, 1], 'x2': [0, 1], 'x3': [0, 1]}, 
+            estimator=BayesianEstimator, prior_type='BDeu', complete_samples_only=False)
 
-for index, row in test.iterrows():
-    pred_shape = row['shape']
+    #Makes the shape estimation uniform if uniformShape is true
+    if(uniformShape):
+        shape_cpd = TabularCPD('shape', 2, [[0.5],[0.5]])
+        graph.add_cpds(shape_cpd)
 
-    nodeNames = list("x" + str(i) for i in range(numberNodes))
+    infer = VariableElimination(graph)
 
-    #Creating a dictionary with all of the node values
-    evidenceDict = {}
+    #Arrays that will contain the actual and predicted values of each of the rows from the test set
+    pred_test = []
+    actu_test = []
 
-    for i in range(numberNodes):
-        evidenceDict[nodeNames[i]] = row[nodeNames[i]]
+    #Evaluating test data
 
-    #Getting the prediction based on the given evidence dictionary
-    pred_shape = str(infer.map_query(variables = ["shape"], evidence= evidenceDict))
-    pred_shape = int(pred_shape[pred_shape.find(':') + 1 : pred_shape.rfind('}')].strip())
+    for index, row in test.iterrows():
+        pred_shape = row['shape']
 
-    #Appending the prediction to the list
-    pred_test.append(pred_shape)
+        #Creates a list of node names, ['x0', 'x1', ...]
+        nodeNames = list("x" + str(i) for i in range(numberNodes))
 
-    #Build query string for the actual value of the shape
-    query_string = ""
+        #Creating a dictionary with all of the node values, {'x0' = 1, 'x1' = 0}
+        evidenceDict = {}
 
-    for key, value in evidenceDict.items():
-        query_string += key + " == " + str(value) + " and "
+        for i in range(numberNodes):
+            evidenceDict[nodeNames[i]] = row[nodeNames[i]]
 
-    query_string = query_string[:query_string.rfind("and")-1]
+        #Getting the prediction based on the given evidence dictionary
+        pred_shape = infer.map_query(variables = ["shape"], evidence= evidenceDict, show_progress=False)['shape']
 
-    #Getting the actual value of the shape given the same activated nodes from the original data set
-    actual_row = dataFile.query(query_string).iloc[0]
 
-    #Appending the actual value to the list
-    actu_test.append(int(actual_row['shape']))
+        #Appending the prediction to the list
+        pred_test.append(pred_shape)
 
-matrix = confusion_matrix(actu_test, pred_test)
+        #Build query string for the actual value of the shape
+        query_string = ""
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        _confusion[i][j] += matrix[i][j] 
+        for key, value in evidenceDict.items():
+            query_string += key + " == " + str(value) + " and "
 
-print(_confusion)
+        query_string = query_string[:query_string.rfind("and")-1]
 
-tn = _confusion[0][0]
-fp = _confusion[0][1]
-fn = _confusion[1][0]
-tp = _confusion[1][1]
+        #Getting the actual value of the shape given the same activated nodes from the original data set
+        actual_row = dataFile.query(query_string).iloc[0]
 
-accuracy = (tp+tn)/(tp+tn+fp+fn)
-sensitivity = tp/(tp+fn)
-specificity = tn/(tn+fn)
+        #Appending the actual value to the list
+        actu_test.append(actual_row['shape'])
+
+    matrix = confusion_matrix(actu_test, pred_test)
+
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix[i])):
+    #         _confusion[i][j] += matrix[i][j] 
+
+    tn = matrix[0][0]
+    fp = matrix[0][1]
+    fn = matrix[1][0]
+    tp = matrix[1][1]
+
+    _accuracy.append((tp+tn)/(tp+tn+fp+fn))
+    _sensitivity.append(tp/(tp+fn))
+    _specificity.append(tn/(tn+fn))
 
 #Calculations are made with 0 being the value of a dot and 1 being the value of a line
 #This means that a false negative is the network predicting a dot when the actual is a line, as dots are negative (0) and lines are positive (1)
-print("-"*30)
-print("Accuracy is {0:.2f}%.".format(accuracy * 100))
-print("Sensitivity is {0:.2f}%.".format(sensitivity * 100))
-print("Specificity is {0:.2f}%.".format(specificity * 100))
-print("-"*30)
+print("-"*50)
+#print("Confusion Matrix: " + str(_confusion))
+print("Accuracy is {0:.2f}%.".format((sum(_accuracy)/len(_accuracy)) * 100))
+print("Sensitivity is {0:.2f}%.".format((sum(_sensitivity)/len(_sensitivity)) * 100))
+print("Specificity is {0:.2f}%.".format((sum(_specificity)/len(_specificity)) * 100))
+print("-"*50)
