c214d41690d0ded84e608b36c203acdcca4cf9b8
Jordan Hinson <jhinson2012@gmail.com>
2024-03-24 13:02:42 -0400

    Defined all base triangles size 3,5,7,9 all rotated

--- DataSimulation/10_10_triangle_square copy.py
+++ DataSimulation/10_10_triangle_square copy.py
@@ -28,6 +28,214 @@ for i in range(2, _numRowCol + 1):
         
     _startingSquares.append(square)
 
+#Creates an array that will store all of the different sized triangles starting at the top left of the input matrix
+_startingTriangles = []
+
+#Base 3 triangels
+
+triangle = list(grid0)
+triangle[1] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[12] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[20] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[1] = 1
+triangle[2] = 1
+triangle[11] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[1] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[21] = 1
+_startingTriangles.append(triangle)
+
+#Base 5 triangels
+
+triangle = list(grid0)
+triangle[2] = 1
+triangle[11] = 1
+triangle[13] = 1
+triangle[20] = 1
+triangle[21] = 1
+triangle[22] = 1
+triangle[23] = 1
+triangle[24] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[20] = 1
+triangle[22] = 1
+triangle[30] = 1
+triangle[31] = 1
+triangle[40] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[2] = 1
+triangle[11] = 1
+triangle[12] = 1
+triangle[20] = 1
+triangle[22] = 1
+triangle[31] = 1
+triangle[32] = 1
+triangle[42] = 1
+_startingTriangles.append(triangle)
+
+#Base 7 triangles
+
+triangle = list(grid0)
+triangle[3] = 1
+triangle[12] = 1
+triangle[14] = 1
+triangle[21] = 1
+triangle[25] = 1
+triangle[30] = 1
+triangle[31] = 1
+triangle[32] = 1
+triangle[33] = 1
+triangle[34] = 1
+triangle[35] = 1
+triangle[36] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[3] = 1
+triangle[12] = 1
+triangle[13] = 1
+triangle[21] = 1
+triangle[23] = 1
+triangle[30] = 1
+triangle[33] = 1
+triangle[41] = 1
+triangle[43] = 1
+triangle[52] = 1
+triangle[53] = 1
+triangle[63] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[1] = 1
+triangle[2] = 1
+triangle[3] = 1
+triangle[4] = 1
+triangle[5] = 1
+triangle[6] = 1
+triangle[11] = 1
+triangle[15] = 1
+triangle[22] = 1
+triangle[24] = 1
+triangle[33] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[20] = 1
+triangle[22] = 1
+triangle[30] = 1
+triangle[33] = 1
+triangle[40] = 1
+triangle[42] = 1
+triangle[50] = 1
+triangle[51] = 1
+triangle[60] = 1
+_startingTriangles.append(triangle)
+
+#Base 9 triangles
+
+triangle = list(grid0)
+triangle[4] = 1
+triangle[13] = 1
+triangle[15] = 1
+triangle[22] = 1
+triangle[26] = 1
+triangle[31] = 1
+triangle[37] = 1
+triangle[40] = 1
+triangle[41] = 1
+triangle[42] = 1
+triangle[43] = 1
+triangle[44] = 1
+triangle[45] = 1
+triangle[46] = 1
+triangle[47] = 1
+triangle[48] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[4] = 1
+triangle[13] = 1
+triangle[14] = 1
+triangle[22] = 1
+triangle[24] = 1
+triangle[31] = 1
+triangle[34] = 1
+triangle[40] = 1
+triangle[44] = 1
+triangle[51] = 1
+triangle[54] = 1
+triangle[62] = 1
+triangle[64] = 1
+triangle[73] = 1
+triangle[74] = 1
+triangle[84] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[1] = 1
+triangle[2] = 1
+triangle[3] = 1
+triangle[4] = 1
+triangle[5] = 1
+triangle[6] = 1
+triangle[7] = 1
+triangle[8] = 1
+triangle[11] = 1
+triangle[17] = 1
+triangle[22] = 1
+triangle[26] = 1
+triangle[33] = 1
+triangle[35] = 1
+triangle[44] = 1
+_startingTriangles.append(triangle)
+
+triangle = list(grid0)
+triangle[0] = 1
+triangle[10] = 1
+triangle[11] = 1
+triangle[20] = 1
+triangle[22] = 1
+triangle[30] = 1
+triangle[33] = 1
+triangle[40] = 1
+triangle[44] = 1
+triangle[50] = 1
+triangle[53] = 1
+triangle[60] = 1
+triangle[62] = 1
+triangle[70] = 1
+triangle[71] = 1
+triangle[80] = 1
+_startingTriangles.append(triangle)
+
 #Creates a dataframe with specified columns
 
 _nodeNames = list("x" + str(i) for i in range(_numRowCol**2))
